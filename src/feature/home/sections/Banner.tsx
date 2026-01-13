d0392af41760fddf26b37ea75100b6ceae9a605b
Yead191 <yead191@gmail.com>
2026-01-13 10:20:35 +0600

    updated banner

--- src/feature/home/sections/Banner.tsx
+++ src/feature/home/sections/Banner.tsx
@@ -7,7 +7,7 @@ export default async function Banner() {
     <section className="bg-[#F7EFDF]">
       <div
         id="banner"
-        className="pt-12 md:pt-0  md:min-h-screen flex flex-col items-center justify-center  container"
+        className="py-12  md:min-h-screen flex flex-col items-center justify-center  container"
       >
         <div className="">
           <Image
@@ -15,14 +15,14 @@ export default async function Banner() {
             alt="Banner Background"
             width={800}
             height={800}
-            className="w-full h-fit"
+            className="w-full h-fit lg:w-[850px]"
           />
           <div className="text-center flex flex-col items-center justify-center">
-            <h1 className="text-primary text-3xl lg:text-4xl 2xl:text-6xl font-medium max-w-[600px] text-center">
+            <h1 className="text-primary text-3xl lg:text-4xl 2xl:text-6xl font-medium max-w-[400px] 2xl:max-w-[600px] text-center">
               More time with the{" "}
               <span className="italic font-thin">pets you love</span>
             </h1>
-            <Button className="mt-6 bg-[#320013]! hover:bg-[#320013]! text-white! font-medium! h-10! lg:h-12! rounded-full! 2xl:w-[200px]! lg:text-[16px]! tracking-wide!">
+            <Button className="mt-6 bg-[#320013]! hover:bg-[#320013]! text-white! font-medium! h-10! 2xl:h-12! rounded-full! lg:w-[200px]! lg:text-[16px]! tracking-wide!">
               Get Started
             </Button>
           </div>
